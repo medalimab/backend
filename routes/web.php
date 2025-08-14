f9df221aaa446673aff520f08e56ce56bc00de72
dali <mabroukdali1925@gmail.com>
2025-08-14 20:41:52 +0200

    feat(routes): add resource routes for Agent CRUD

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoutingController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\AgentController;
 
 require __DIR__ . '/auth.php';
 
@@ -25,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/properties/{id}', [PropertyController::class, 'show'])->name('properties.show');
     Route::resource('properties', PropertyController::class);
     Route::post('/properties/search', [PropertyController::class, 'search'])->name('properties.search');
+
+    Route::resource('agents', AgentController::class);
 
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
