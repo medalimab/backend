57880aae40a1ff449c5ee90024e4c3086876ca3f
dali <mabroukdali1925@gmail.com>
2025-08-15 14:18:00 +0200

    feat(migration): ajout des champs dld_permit_number et dld_permit_qr à la table properties pour la gestion du DLD permit et du QR code

---
+++ database/migrations/2025_08_14_190734_add_dld_permit_fields_to_properties_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->string('dld_permit_number')->nullable();
+            $table->string('dld_permit_qr')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropColumn('dld_permit_number');
+            $table->dropColumn('dld_permit_qr');
+        });
+    }
+};
