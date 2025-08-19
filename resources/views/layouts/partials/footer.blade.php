4656c3aaec684e3c6f32cb1949d22d1a2fa331fe
dali <mabroukdali1925@gmail.com>
2025-08-19 14:32:11 +0200

    update footeer dash

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,8 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 text-center">
-                <script>document.write(new Date().getFullYear())</script> &copy; Lahomes. Crafted by <iconify-icon icon="solar:hearts-bold-duotone" class="fs-18 align-middle text-danger"></iconify-icon> <a
-                    href="" class="fw-bold footer-text" target="_blank">Techzaa</a>
+                © 2025 All Rights Reserved - Developed by Ahdaf Digital
             </div>
         </div>
     </div>
