1351389d9aece2cd1b2ca7e15c4755416da3ade8
dali <mabroukdali1925@gmail.com>
2025-08-23 16:05:57 +0200

    padding  1000

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -5,7 +5,7 @@
       padding-left: 0 !important;
       margin-left: 0 !important;
       margin-right: 0 !important;
-      padding-right: 100 !important;
+      padding-right: 1000 !important;
     }
     .card {
       margin-left: 0 !important;
